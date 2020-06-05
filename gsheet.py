5001ae84072dc1ed87e11291722a1291c20bd09d
vovailchenko <vova.ilchenko@gmail.com>
2020-06-05 23:13:23 +0300

    added 2 funcs for sheets

--- gsheet.py
+++ gsheet.py
@@ -20,12 +20,30 @@ def get_offices(sh):
 def get_users(sh):
     credentials_worksheet = sh.worksheet("Credentials")
     users = credentials_worksheet.get_all_values()
-    #print(users)
+    # print(users)
     return users
+
 
 def persist_user_id(sh, row, id):
     credentials_worksheet = sh.worksheet("Credentials")
     credentials_worksheet.update_cell(row, 4, id)
-    #users = credentials_worksheet.get_all_values()
-    #print(users)
+    # users = credentials_worksheet.get_all_values()
+    # print(users)
+    return
+
+
+# this gets a list of lists from a gsheet.
+# sheet is an instance of google sheet - this is returned by add_gsheet somewhere in init section
+# worksheet is a sheet name, e.g. "RESERVATIONS", "Credentials"
+def get_lists(sheet, worksheet):
+    wksheet = sheet.worksheet(worksheet)
+    # [1:] filter is needed to filted out headers row, as requested by Mr. Kroz
+    return wksheet.get_all_values()[1:]
+
+
+# todo add description
+# values should be a list
+def add_row(sheet, worksheet, values):
+    wksheet = sheet.worksheet(worksheet)
+    wksheet.append_row(values)
     return
